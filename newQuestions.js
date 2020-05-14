e2ac9566568bc9a2d78874c2c5594597ada52cbe
Stefan Bettaglio <sbettaglio@gmail.com>
2020-05-14 13:16:14 -0400

    improved module with event emitter

--- newQuestions.js
+++ newQuestions.js
@@ -6,4 +6,15 @@ const questions = [
   "What are you going to do with node js? ",
 ];
 
-collectAnswers(questions);
+const answerEvents = collectAnswers(questions);
+
+answerEvents.on("answer", (answer) => {
+  console.log(`question answered: ${answer}`);
+});
+
+answerEvents.on("complete", (answers) => {
+  console.log("Thank you for your answers. ");
+  console.log(answers);
+});
+
+answerEvents.on("complete", () => process.exit());
